f9294369fe14ebfa1860f568ff536ea5626113ab
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-01 02:31:33 -0400

    Update pool.ts

--- src/utils/pool.ts
+++ src/utils/pool.ts
@@ -5,3 +5,4 @@ const pool = fs.readFileSync('./bytecode/pool.wasm');
 
 const poolBytecodeHash = bitcoin.crypto.hash256(pool);
 console.log('Pool bytecode hash:', poolBytecodeHash.toString('hex'), Array.from(poolBytecodeHash));
+
